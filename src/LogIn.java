598c9362640dd04110b043af44582eae963a2d2c
EDiaz210 <elkin.diaz@epn.du.ec>
2025-01-23 09:07:05 -0500

    login finish

---
+++ src/LogIn.java
@@ -0,0 +1,72 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class LogIn {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JLabel ImagenLogin;
+    private JComboBox comboBox1;
+    private JButton ingresarButton;
+    public JPanel Login;
+
+
+    public LogIn() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try (Connection connection = ConexionBD.getConnection()) {
+                    Statement statement = connection.createStatement();
+                    String query = "SELECT * FROM usuarios WHERE usuario = '" + textField1.getText() + "' AND contrasenia = '" + new String(passwordField1.getPassword()) + "' AND rol = '" + comboBox1.getSelectedItem().toString() + "'";
+                    ResultSet resultSet = statement.executeQuery(query);
+
+
+                    if(resultSet.next()) {
+                        if (comboBox1.getSelectedItem().equals(resultSet.getString("rol"))){
+                            if(textField1.getText().equals(resultSet.getString("usuario")) && new String(passwordField1.getPassword()).equals(resultSet.getString("contraseña"))) {
+                                if(resultSet.getString("rol").equals("administrador")){
+                                    System.out.println("Ingresaste a modo administrador ");
+                                    JFrame frame = new JFrame();
+                                    frame.setTitle("Login");
+                                    frame.setSize(350, 300);
+                                    frame.setContentPane(new MenuAdmin().menu);
+                                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                    frame.setVisible(true);
+                                    ((JFrame) SwingUtilities.getWindowAncestor(ingresarButton)).dispose();
+
+                                }else if(resultSet.getString("rol").equals("usuario")){
+                                    System.out.println("Ingresaste a modo usuario ");
+                                    JFrame frame = new JFrame();
+                                    frame.setTitle("Login");
+                                    frame.setSize(350, 300);
+                                    frame.setContentPane(new MenuMiembros().menu);
+                                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                    frame.setVisible(true);
+                                    ((JFrame) SwingUtilities.getWindowAncestor(ingresarButton)).dispose();
+                                }
+
+                            }
+                        }else {
+                            JOptionPane.showInputDialog("Digite correctamente las credenciales");
+                        }
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+
+                }
+            }
+        });
+    }
+
+    public static class ConexionBD {
+        private static final String url = "jdbc:mysql://uu0mrbdpzuyyx7gg:qQKJg3BOiryNGsrQdFYP@bjbwtijcisxcuwxsxuat-mysql.services.clever-cloud.com:3306/bjbwtijcisxcuwxsxuat";
+        private static final String username = "uu0mrbdpzuyyx7gg";
+        private static final String password = "qQKJg3BOiryNGsrQdFYP";
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(url,username, password);
+        }
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame jp = new JFrame();
-        jp.setContentPane();
+        jp.setContentPane(new LogIn().Login);
         jp.setSize(200,200);
         jp.setVisible(true);
-        ((JFrame)SwingUtilities.getWindowAncestor()).dispose();
 
 
     }
