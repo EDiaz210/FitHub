ce2d7011a495fb01d208db6fd7862971fa3accf5
2Goddie7 <diego.mullo@epn.edu.ec>
2025-02-05 19:37:47 -0500

    Pestaña miembros

--- src/Menus/MenuMiembros.java
+++ src/Menus/MenuMiembros.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class MenuMiembros {
     public JPanel menu;
+    private JTabbedPane tabbedPane1;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JButton pagarButton;
 }
