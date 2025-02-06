46011a1b879140b8964efa2a575a984c293df914
2Goddie7 <diego.mullo@epn.edu.ec>
2025-02-06 15:32:07 -0500

    miembros

--- src/Menus/MenuMiembros.java
+++ src/Menus/MenuMiembros.java
@@ -9,4 +9,13 @@ public class MenuMiembros {
     private JTextArea textArea2;
     private JTextArea textArea3;
     private JButton pagarButton;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JList list4;
+    private JList list5;
+    private JList list6;
+    private JButton comprarButton;
+    private JButton comprarButton1;
+    private JButton comprarButton2;
 }
